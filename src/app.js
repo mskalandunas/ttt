3009204f2bbabb04239813ffcc403fa3996152bc
Mike Skalandunas <mskalandunas@gmail.com>
2022-01-10 22:24:48 -0800

    system selects starting player

--- src/app.js
+++ src/app.js
@@ -1,6 +1,31 @@
 (function() {
   'use strict';
 
+  /* Selectors */
+  const PLAYER_ONE_INPUT = 'Player1NameInput';
+  const PLAYER_TWO_INPUT = 'Player2NameInput';
+  const PlAYER_ONE_LABEL = `[for=${PLAYER_ONE_INPUT}]`;
+  const PLAYER_TWO_LABEL = `[for=${PLAYER_TWO_INPUT}]`;
+  const START_BUTTON = 'StartGame';
+
+  const state = {
+    currentTurn: 0,
+    players: [null, null]
+  };
+
+  function start() {
+    [
+      PlAYER_ONE_LABEL,
+      PLAYER_TWO_LABEL
+    ].forEach((selector, i) => {
+      const el = document.querySelector(selector);
+
+      state.players[i] = el.textContent;
+
+      el.setAttribute('disabled', true);
+    });
+  }
+
   function updateName(evt) {
     document
       .querySelector(`[for=${evt.target.id}]`)
@@ -9,9 +34,12 @@
 
   /* DOM */
   document
-    .getElementById('Player1NameInput')
+    .getElementById(PLAYER_ONE_INPUT)
     .addEventListener('blur', updateName);
-    document
-    .getElementById('Player2NameInput')
+  document
+    .getElementById(PLAYER_TWO_INPUT)
     .addEventListener('blur', updateName);
+  document
+    .getElementById(START_BUTTON)
+    .addEventListener('click', start);
 })();
