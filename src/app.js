7717bd74984e2eb6c2062841b2d691ba89e3c834
Mike Skalandunas <mskalandunas@gmail.com>
2022-01-14 23:52:39 -0800

    remove duplicate validation logic

--- src/app.js
+++ src/app.js
@@ -34,36 +34,6 @@ function initialize() {
   new GameModule(DEFAULT_GAME_SIZE);
 }
 
-function validate(board, size) {
-  // check columns
-  for (let i = 0; i < size; i++) {
-    const columnSet = new Set([board[i]]);
-
-    for (let j = 0; j < board.length; j += size) {
-      columnSet.add(board[j]);
-    }
-
-    if (columnSet.size === 1) {
-      return true;
-    }
-  }
-
-  // check rows
-  for (let i = 0; i < board.length; i += size) {
-    const rowSet = new Set([board[i]]);
-    
-    for (let j = 0; j < size; j++) {
-      rowSet.add(board[j]);
-    }
-
-    if (rowSet.size === 1) {
-      return true;
-    }
-  }
-
-  return  false;
-}
-
 function updateBoard(board, index, currentTurn) {
   return [
     ...board.slice(0, index),
